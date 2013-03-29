4875afdade0bdd688cb7720c7c2a729734af15c4
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-03-29 14:55:06 +0900

    m
    v-1.0
    ---
    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CR5R::Application.config.secret_token = '525a220e04bed593a82460ecdd4da5171c8ec1f894b0eeaf04619b6a474ee5dfd8156014d9fbe46cd085c1fb367ff5fa6787c8ecdfd256064a8a1e0d95dbb2d5'
