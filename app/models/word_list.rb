98863e50ce345113512b959670b0b26e17c1f859
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-04-06 17:21:55 +0900

    B7(m)
    v-2.1a-p2-t1
    ---
    1. Forgot to merge master to this. Merging...

--- app/models/word_list.rb
+++ app/models/word_list.rb
@@ -1,4 +1,18 @@
 class WordList < ActiveRecord::Base
+  
+  # validates_uniqueness_of :text_id, :scope => :word_id
+  # validate :check
+  
   belongs_to :text
   belongs_to :word
-end
+  
+  private
+  def check
+    
+    
+    # logout("wordlist id=" + self.id.to_s + "\n")
+    # logout("wordlist text_id=" + self.text_id.to_s + "\n")
+    
+  end#def check
+  
+end#class WordList < ActiveRecord::Base
