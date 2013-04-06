afd5313dff6c811b16f3cd9031b61ae954e2dbe3
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-04-06 15:28:28 +0900

    B7(m)
    v-2.0
    ---
    1. Pattern match => Done

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -40,7 +40,83 @@ class AdminController < ApplicationController
   
   def word_list
     
+    words = Word.find(:all)
+    texts = Text.limit(3)
+    text = texts[2]
+    # texts = Text.limit(2)
+    # text = texts[1]
     
+    @message = ""
     
-  end
-end
+    logout("=============================================")
+    logout("text.id=" + text.id.to_s)
+    
+    @message = "text.id=" + text.id.to_s
+    
+    if words != nil
+      
+      logout("words.length=" + words.length.to_s)
+      
+      @message = "words.length=" + words.length.to_s
+      
+      # for i in (1..words.length)
+      for i in (0..words.length - 1)
+        
+        word = words[i]
+        
+        if word != nil
+          
+          logout(word.w1 + "\n")
+          
+          res = isIn?(text, word.w1, @message)
+          
+          
+          
+        else
+          
+          logout("nil" + "\n")
+          
+          @message += "nil" + "\n"
+          
+        end
+        logout(word.w1 + "\n")
+        
+      end#for words.length
+      
+      # @message = "Done"
+      
+    else
+      
+      @message = "words != nil"
+      
+    end#if words != nil
+    
+  end#def word_list
+  
+  def isIn?(text, target_word, message)
+    
+    logout("target_word=" + target_word)
+    
+    # regex = Regexp.new("^this is regexp")
+    regex = Regexp.new(target_word)
+    
+    if text.text =~ regex
+      
+      logout("Match: " + target_word)
+      
+      message += "Match: " + target_word
+      
+      return true
+      
+    else
+
+      logout("Doesn't match: " + target_word)
+      message += "Doesn't match: " + target_word
+      
+      return false
+      
+    end#if text.text =~ regex
+    
+  end#def isIn?(text, target_word)
+  
+end#class AdminController < ApplicationController
